36b64de6efff7d089bcf2488386b1fec55f4be25
ajiteveryone8 <ajiteveryone8@gmail.com>
2024-12-18 11:17:14 +0000

    First home route created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello to codespace');
+const app = require('./app')
+
+const {PORT} = process.env
+
+console.log(PORT)
+
+app.listen(PORT, () =>{
+    console.log(`server is running is at port : ${PORT}`)
+})
